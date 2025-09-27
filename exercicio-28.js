25c373711fe58be4a824d279f7ed2f1a4bbe299a
unknown <felipemendes5552@gmail.com>
2025-09-27 02:23:34 -0300

    Usando um laço while, calcule e imprima a soma de todos os números de 1 a 100.

---
+++ exercicio-28.js
@@ -0,0 +1,13 @@
+//Exercício 28: Usando um laço while, calcule e imprima a soma de todos os números de 1 a 100.
+
+let n = 1
+let soma = 0
+
+while (n <= 100) {
+     soma = (soma = soma + n)
+    n++
+
+    console.log (soma)
+}
+    
+   
