3522c40e807a0c5910414eaf13217c7068ac9ec6
felipem5552 <felipemendes5552@gmail.com>
2025-10-08 22:20:51 -0300

    Update exercicio-18.js

--- exercicio-18.js
+++ exercicio-18.js
@@ -1,6 +1,6 @@
 //Exercício 18: Verifique se uma pessoa pode dirigir com base na idade.
 
-let idade = parseInt(prompt("Digite sua idade para saber se você pode dirigir:"));
+let idade = parseInt(prompt("Digite sua idade para saber se você pode dirigir:"))
 
 
 if (idade >= 18) {
